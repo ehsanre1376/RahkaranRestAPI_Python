7791f363b766ee88ea4b88207ff617ee44143825
ehsanre1376 <103402757+ehsanre1376@users.noreply.github.com>
2024-02-18 19:45:44 +0330

    1

---
+++ login.py
@@ -0,0 +1,40 @@
+import requests
+import rsa
+import json
+import binascii
+G_BaseURL = "http://127.0.0.1/PortfolioDEV"
+G_UserName ="admin"
+G_PassWord = "admin"
+def hex_string_to_bytes(hex_string):
+    return binascii.unhexlify(hex_string)
+
+def bytes_to_hex_string(byte_array):
+    return binascii.hexlify(byte_array).decode()
+
+def login(user_name = G_UserName, password=G_PassWord):
+    url = G_BaseURL+"/Services/Framework/AuthenticationService.svc"
+    session_url = url + "/session"
+    login_url = url + "/login"
+
+    response = requests.get(session_url)
+    if response.status_code != 200:
+        raise Exception('GET /session {}'.format(response.status_code))
+    session = json.loads(response.text)
+    m = hex_string_to_bytes(session['rsa']['M'])
+    e = hex_string_to_bytes(session['rsa']['E'])
+    rsa_key = rsa.PublicKey(int.from_bytes(m, byteorder='big'), int.from_bytes(e, byteorder='big'))
+    session_plus_password = session['id'] + "**" + password
+    encrypted_password = rsa.encrypt(session_plus_password.encode(), rsa_key)
+    headers = {'content-Type': 'application/json'}
+    data = {
+        'sessionId': session['id'],
+        'username': user_name,
+        'password': bytes_to_hex_string(encrypted_password)
+    }
+    response = requests.post(login_url, headers=headers, data=json.dumps(data))
+    if response.status_code != 200:
+        raise Exception('POST /login {}'.format(response.status_code))
+    session = response.headers['Set-Cookie'].split(',')[2].split(';')[0].strip()
+    return session
+session = login()
+print(session)
