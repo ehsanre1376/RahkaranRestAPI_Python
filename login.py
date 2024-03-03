6f79e318a2af2380581fe3964a4718ac6d82a5ab
Ehsan Rezaei <ehsanre@systemgroup.net>
2024-03-03 19:51:39 +0330

    10

--- login.py
+++ login.py
@@ -7,36 +7,54 @@ import binascii
 import tempfile
 import os
 from datetime import datetime
-G_session =""
+
+G_session = ""
 G_ExpireDate = datetime(2020, 3, 15)
 G_protocol = "http"
 G_ServerName = "127.0.0.1"
-G_ServerPort="80"
+G_ServerPort = "80"
 G_RahkaranName = "PortfolioDEV"
-G_BaseURL = G_protocol + "://" + G_ServerName +":"+"80"+ "/" + G_RahkaranName
+G_BaseURL = G_protocol + "://" + G_ServerName + ":" + "80" + "/" + G_RahkaranName
 G_AuthenticationName = "sg-auth-"
-G_UserName ="admin"
+G_UserName = "admin"
 G_PassWord = "admin"
+
 
 def hex_string_to_bytes(hex_string):
     return binascii.unhexlify(hex_string)
 
+
 def bytes_to_hex_string(byte_array):
     return binascii.hexlify(byte_array).decode()
 
-def login(user_name = G_UserName, password=G_PassWord):
+
+def login(user_name=G_UserName, password=G_PassWord):
     global G_ExpireDate
     global G_session
-    # with open(
-    #    tempfile.gettempdir(), G_AuthenticationName + G_RahkaranName + ".txt", "r") as file:
-    #    content = file.read()
-    with open(
-        r"C:\Users\EHSANRE\AppData\Local\Temp\sg-auth-PortfolioDEV.txt", "r"
-    ) as file:
-        content = file.read()
-    if datetime.now() < G_ExpireDate :
-        return G_session,G_ExpireDate
-    url = G_BaseURL+"/Services/Framework/AuthenticationService.svc"
+    try:
+        with open(
+            os.path.join(
+                tempfile.gettempdir(), G_AuthenticationName + G_RahkaranName + ".txt"
+            ),
+            "r",
+            encoding="utf-8",
+        ) as file:
+            content = file.readlines()
+            G_session = content[0]
+            G_ExpireDate =datetime.strptime(content[1].strip(), "%d-%b-%Y %H:%M:%S")
+            print(G_ExpireDate)
+    except FileNotFoundError:
+        print(f"The file {os.path.join(
+                tempfile.gettempdir(), G_AuthenticationName + G_RahkaranName + ".txt"
+            )} does not exist.")
+    except IOError:
+        print("An error occurred while trying to read the file.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    if datetime.now() < G_ExpireDate:
+        return G_session, G_ExpireDate
+    url = G_BaseURL + "/Services/Framework/AuthenticationService.svc"
     session_url = url + "/session"
     login_url = url + "/login"
 
@@ -44,29 +62,41 @@ def login(user_name = G_UserName, password=G_PassWord):
     if response.status_code != 200:
         raise ValueError(f"GET /session {response.status_code}")
     session = json.loads(response.text)
-    m = hex_string_to_bytes(session['rsa']['M'])
-    e = hex_string_to_bytes(session['rsa']['E'])
-    rsa_key = rsa.PublicKey(int.from_bytes(m, byteorder='big'), int.from_bytes(e, byteorder='big'))
-    session_plus_password = session['id'] + "**" + password
+    m = hex_string_to_bytes(session["rsa"]["M"])
+    e = hex_string_to_bytes(session["rsa"]["E"])
+    rsa_key = rsa.PublicKey(
+        int.from_bytes(m, byteorder="big"), int.from_bytes(e, byteorder="big")
+    )
+    session_plus_password = session["id"] + "**" + password
     encrypted_password = rsa.encrypt(session_plus_password.encode(), rsa_key)
-    headers = {'content-Type': 'application/json'}
+    headers = {"content-Type": "application/json"}
     data = {
-        'sessionId': session['id'],
-        'username': user_name,
-        'password': bytes_to_hex_string(encrypted_password)
+        "sessionId": session["id"],
+        "username": user_name,
+        "password": bytes_to_hex_string(encrypted_password),
     }
-    response = requests.post(login_url, headers=headers, data=json.dumps(data),timeout=10)
+    response = requests.post(
+        login_url, headers=headers, data=json.dumps(data), timeout=10
+    )
     if response.status_code != 200:
         raise ValueError(f"POST /login {response.status_code}")
-    session = response.headers['Set-Cookie'].split(',')[2].split(';')[0].strip()
+    session = response.headers["Set-Cookie"].split(",")[2].split(";")[0].strip()
     ExpireDate = response.headers["Set-Cookie"].split(",")[1].split(";")[0].strip()
     ExpireDate = datetime.strptime(ExpireDate, "%d-%b-%Y %H:%M:%S %Z")
     G_session = session
     G_ExpireDate = ExpireDate
-    with open(os.path.join(tempfile.gettempdir(),G_AuthenticationName+G_RahkaranName+".txt" ), "w",encoding="utf-8") as f:
-        f.write(G_session + "\n")
-        f.write(G_ExpireDate.strftime('%y/%m/%d,%H/%M/%S') + "\n")
-    return session,ExpireDate
-session,ExpireDate = login()
+    with open(
+        os.path.join(
+            tempfile.gettempdir(), G_AuthenticationName + G_RahkaranName + ".txt"
+        ),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        f.write(G_session+"\n" )
+        f.write(G_ExpireDate.strftime("%d-%b-%Y %H:%M:%S %Z"))
+    return session, ExpireDate
+
+
+session, ExpireDate = login()
 
 print(session, ExpireDate)
